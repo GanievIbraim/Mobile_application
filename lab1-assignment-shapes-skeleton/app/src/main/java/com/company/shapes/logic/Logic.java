45d53959dcdccc73a9431522a9d3f08b850b763d
Ibraim Ganiev <ganiev.i.i.21@gmail.com>
2024-02-15 20:13:21 +0300

    lab_1

--- lab1-assignment-shapes-skeleton/app/src/main/java/com/company/shapes/logic/Logic.java
+++ lab1-assignment-shapes-skeleton/app/src/main/java/com/company/shapes/logic/Logic.java
@@ -161,5 +161,35 @@ public class Logic
     }
 
     // TODO -- add your code here
-
+    public static double rectangleArea(double length, double width){
+        return length * width;
+    }
+    public static double rectanglePerimeter(double length, double width) {
+        return (length + width) * 2;
+    }
+    public static double circleArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+    public static double circleCircumference(double radius) {
+        return Math.PI * 2 * radius;
+    }
+    public static double rightTriangleArea (double base, double height) {
+        return 0.5 * base * height;
+    }
+    public static double rightTrianglePerimeter(double base, double height){
+        double sumAandB = Math.pow(base, 2) + Math.pow(height, 2);
+        return base + height + (Math.sqrt(sumAandB));
+    }
+    public static double boxVolume(double length, double width, double depth){
+        return length * width * depth;
+    }
+    public static double boxSurfaceArea (double length, double width, double depth){
+        return (2 * length * width) + (2 * length * depth)  + (2 * depth * width);
+    }
+    public static double sphereVolume (double radius) {
+        return (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+    }
+    public static double sphereSurfaceArea(double radius) {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
 }
